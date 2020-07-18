c028fe4517ff9e5e522235e98bb86f0922116280
SemihDurmus <ssdurmus@yahoo.com>
2020-07-18 08:05:53 +0200

    New assignment added

---
+++ Assignment_14_Fizz_Buzz.py
@@ -0,0 +1,20 @@
+
+# Assignment_14_Fizz_Buzz
+"""
+Print numbers from 1 to 100 inclusively following these instructions:
+if a number is multiple of 3, print "Fizz" instead of this number,
+if a number is multiple of 5, print "Buzz" instead of this number,
+for numbers that are multiples of both 3 and 5, print "FizzBuzz",
+print the rest of the numbers unchanged.
+Output each value on a separate line.
+"""
+num_list = list(range(1,101))
+for i in num_list:
+    if i % 15 == 0:
+        print("FizzBuzz")
+    elif i % 3 == 0:
+        print("Fizz")
+    elif i % 5 == 0:
+        print("Buzz")
+    else:
+        print(i)
