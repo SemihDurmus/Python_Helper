a9b393f8827f5078687bfcb7afafe007252e9bb8
SemihDurmus <ssdurmus@yahoo.com>
2020-08-13 18:52:36 +0200

    Push from VS Code

---
+++ Assignment_18_Most_Frequent_Element.py
@@ -0,0 +1,18 @@
+# Assignment_18_Most_Frequent_Element
+
+"""Given a list, return the most frequent (repeating) element."""
+
+def most_freq(given_list):
+    rep = 0
+
+    result = given_list[0]
+    
+    for i in given_list:
+        
+        live_rep = given_list.count(i)
+        
+        if live_rep > rep:
+            rep = live_rep
+            result = i    
+            
+    return result
